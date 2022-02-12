02524bc90d5044d90ac711616724559bbcb51759
Ted Dawson <dawson.ted@gmail.com>
2022-02-12 20:45:47 +0100

    add statistics

--- sigsaw.py
+++ sigsaw.py
@@ -39,4 +39,15 @@ g = open(r"C:\Users\edawson4\Documents\emails\pattied.txt","w")
 g.write(pattied)
 g.close
 
+# produce statistics
+
+words = pattied.split(' ')
+total_words = len(words)
+real_words = depattied.split(' ')
+total_real_words = len(real_words)
+cut_words = total_words - total_real_words
+percent_bs = round(((cut_words / total_words) * 100), 2)
+print(percent_bs, "percent of words in this email chain were unnecessary!\n Of ", total_words, "total words,", cut_words, "were self-aggrandazing email signature bs.\n A version of the exchange with these words removed has been saved to local storage.")
+
+
 # future change--prompt for file names, etc
